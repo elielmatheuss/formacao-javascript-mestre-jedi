395f212f63f5475103dc3f03a9c1fbe53617ba63
elielmatheuss <elielmatheuss@hotmail.com>
2022-08-17 13:50:39 -0300

    variavel string

---
+++ tipo-de-dados-string/js/string.js
@@ -0,0 +1,14 @@
+var nome = "Eliel"
+var sobreNome = "Mateus Oliveira"
+var nomeCompleto = nome + " " + sobreNome
+
+console.log("Nome Completo: "+ nomeCompleto)
+
+var numeroString = "20" + 10
+var numeroStringInv = 10 + "50"
+
+console.log("Valor: " + numeroString + numeroStringInv)
+
+var stringVar = false
+
+console.log("Tipo: " + typeof stringVar)
